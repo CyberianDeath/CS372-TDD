8f7ad1129f046b71b360d01056440ddb201fb5ee
chale11 <chale11@alaska.edu>
2020-03-21 17:08:04 -0800

    initial drafts

---
+++ String-Calculator/stringCalc.cpp
@@ -0,0 +1,11 @@
+//Chaweinta Hale
+// Started:     March, 21 2020
+// Modified:    March, 22 2020
+//TDD practice with String Calculator
+// Based on www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
+
+#include "stringCalc.h"
+#include "catch.hpp"
+
+stringCalc::stringCalc(){
+}
---
+++ String-Calculator/stringCalc.h
@@ -0,0 +1,17 @@
+//Chaweinta Hale
+// Started:     March, 21 2020
+// Modified:    March, 22 2020
+//TDD practice with String Calculator
+// Based on www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
+#ifndef FILE_STRINGCALC_H_INCLUDED
+#define FILE_STRINGCALC_H_INCLUDED
+
+#include <string>
+
+class stringCalc{
+public:
+    stringCalc(std::string);
+private:
+
+}
+#endif
